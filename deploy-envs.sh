1e5e6b0b24cf1e9ea2e85ceaf682a1b950c8ede0
kpconnell1132 <kevin@connells.net>
2017-04-30 16:36:30 -0400

    port 5000

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -7,7 +7,7 @@ export IMAGE_VERSION=latest
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_ECS_CLUSTER_NAME=ecscluster1
 export AWS_VIRTUAL_HOST=elb1-1327534490.us-east-1.elb.amazonaws.com
-export AWS_LOAD_BALANCER=loadBalancerName=elb1,containerName=netcoreapps-stackapis,containerPort=80
+export AWS_LOAD_BALANCER=loadBalancerName=elb1,containerName=netcoreapps-stackapis,containerPort=5000
 export AWS_ECS_ELB_ROLE=arn:aws:iam::025075093992:role/ecsInstanceRole
 
 # set any sensitive information in travis-ci encrypted project settings:
