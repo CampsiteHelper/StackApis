547196f04ff5d04666f8980928bbd2346174938b
kpconnell1132 <kevin@connells.net>
2017-04-30 15:07:58 -0400

    vars

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -5,8 +5,8 @@ export IMAGE_NAME=netcoreapps-stackapis
 export IMAGE_VERSION=latest
 
 export AWS_DEFAULT_REGION=us-east-1
-export AWS_ECS_CLUSTER_NAME=default
-export AWS_VIRTUAL_HOST=stackapis.netcore.io
+export AWS_ECS_CLUSTER_NAME=ecscluster1
+export AWS_VIRTUAL_HOST=elb1-1327534490.us-east-1.elb.amazonaws.com
 
 # set any sensitive information in travis-ci encrypted project settings:
 # required: AWS_ACCOUNT_NUMBER, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
